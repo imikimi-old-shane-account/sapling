f95659e8879654d2ca428913aba5f87dbab1ba3b
Jason Strutz <navyrain@navyrain.net>
2011-12-12 15:19:02 -0800

    Fix string/symbol bugs in AR adapter

--- lib/sapling/active_record.rb
+++ lib/sapling/active_record.rb
@@ -47,25 +47,25 @@ module Sapling
       def activate_user(feature, user)
         Model.transaction do
           deactivate_user(feature, user)
-          Model.create(:feature => feature, :user_id => user.id)
+          Model.create(:feature => feature.to_s, :user_id => user.id)
         end
       end
 
       def deactivate_user(feature, user)
-        Model.delete_all ["feature = ? AND percentage IS NULL and user_id = ?",feature,user.id]
+        Model.delete_all ["feature = ? AND percentage IS NULL and user_id = ?",feature.to_s,user.id]
       end
 
       def activate_percentage(feature, percentage)
         raise "invalid percentage #{percentage.inspect}" unless percentage.kind_of?(Integer) && percentage>=0 && percentage<=100
         Model.transaction do
           deactivate_percentage(feature)
-          Model.create(:feature => feature, :percentage => percentage)
+          Model.create(:feature => feature.to_s, :percentage => percentage)
         end
       end
 
       def deactivate_percentage(feature)
         Model.delete_all ["feature = ? AND percentage IS NOT NULL AND user_id IS NULL",
-          feature
+          feature.to_s
         ]
       end
     end
