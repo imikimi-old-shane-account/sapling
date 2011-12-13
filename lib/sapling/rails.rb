1010097d1b93073bde3020dfe4639de7eb5fa02f
Jason Strutz <navyrain@navyrain.net>
2011-12-13 13:59:57 -0800

    Helpers now injected into rails

--- lib/sapling/rails.rb
+++ lib/sapling/rails.rb
@@ -1,2 +1,8 @@
 require 'sapling/rails/action_controller'
-# require 'sapling/rails/sapling_helper'
+require 'sapling/rails/view_helpers'
+
+if Rails.version =~ /^3\./
+  require 'sapling/rails/railtie'
+else
+  ActionView::Base.send :include, Sapling::ViewHelpers
+end
