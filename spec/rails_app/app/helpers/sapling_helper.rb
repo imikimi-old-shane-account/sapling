6f1fbf511d20a364ae742dbbeed2f1bad75d52dc
shanebdavis <shanebdavis@gmail.com>
2011-12-13 13:32:04 -0800

    default to not showing features if not in database

--- spec/rails_app/app/helpers/sapling_helper.rb
+++ spec/rails_app/app/helpers/sapling_helper.rb
@@ -2,21 +2,21 @@
 module SaplingHelper
   # include ActionView::Helpers::CaptureHelper
   def feature_on(feature, &block)
-    feature_block(sapling_css_generator.css_class(feature, true), &block)
+    feature_block(sapling_css_generator.css_class(feature, true), "style='display:none;'", &block)
   end
 
   def feature_off(feature, &block)
-    feature_block(sapling_css_generator.css_class(feature, false), &block)    
+    feature_block(sapling_css_generator.css_class(feature, false), nil, &block)
   end
-  
-  
+
+
   private
-  def feature_block(css_class, &block)
-    concat "<span class=\"#{css_class}\">"
-    block.call 
+  def feature_block(css_class, params, &block)
+    concat "<span class=\"#{css_class}\" #{params}>"
+    block.call
     concat "</span>"
   end
-  
+
 
 end
 
